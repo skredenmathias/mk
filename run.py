a739d6736743128cc99e23dedd288b6dcf123c6a
skredenmathias <skredenmathias@users.noreply.github.com>
2020-11-11 10:27:44 +0100

    updated app name, social credentials

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ from pages import index, predictions, insights, process
 
 # Navbar docs: https://dash-bootstrap-components.opensource.faculty.ai/l/components/navbar
 navbar = dbc.NavbarSimple(
-    brand='YOUR APP NAME',
+    brand='Markedskraft Log Analyse',
     brand_href='/', 
     children=[
         dbc.NavItem(dcc.Link('Predictions', href='/predictions', className='nav-link')), 
@@ -35,11 +35,11 @@ footer = dbc.Container(
         dbc.Col(
             html.P(
                 [
-                    html.Span('Your Name', className='mr-2'), 
-                    html.A(html.I(className='fas fa-envelope-square mr-1'), href='mailto:<you>@<provider>.com'), 
-                    html.A(html.I(className='fab fa-github-square mr-1'), href='https://github.com/<you>/<repo>'), 
-                    html.A(html.I(className='fab fa-linkedin mr-1'), href='https://www.linkedin.com/in/<you>/'), 
-                    html.A(html.I(className='fab fa-twitter-square mr-1'), href='https://twitter.com/<you>'), 
+                    html.Span('Mathias Ragnarson Skreden', className='mr-2'), 
+                    html.A(html.I(className='fas fa-envelope-square mr-1'), href='mailto:<mathias.skreden>@<gmail>.com'), 
+                    html.A(html.I(className='fab fa-github-square mr-1'), href='https://github.com/<skredenmathias>/<MK>'), 
+                    html.A(html.I(className='fab fa-linkedin mr-1'), href='https://www.linkedin.com/in/<skredenmathias>/'), 
+                    html.A(html.I(className='fab fa-twitter-square mr-1'), href='https://twitter.com/<skredenmathias>'), 
                 ], 
                 className='lead'
             )

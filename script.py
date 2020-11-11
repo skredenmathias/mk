c5108ed85798532c8e33dd6bcb3b2461cb0b68e3
skredenmathias <skredenmathias@users.noreply.github.com>
2020-11-11 10:20:54 +0100

    script to download SimpleLog.bak, password omitted

---
+++ script.py
@@ -0,0 +1,14 @@
+import pysftp
+
+cnopts = pysftp.CnOpts()
+cnopts.hostkeys = None                      # disable host key checking
+
+host = "ftp2.markedskraft.com"       #hard-coded
+password = "wZGQtkwMDc6tcRRn"               #hard-coded
+username = "mathias.skreden"                #hard-coded
+
+with pysftp.Connection(host, username=username, password=password, cnopts=cnopts) as sftp:
+    files = sftp.listdir_attr(".")
+    for f in files:
+        print(f)
+    sftp.get_d('.', localdir='./data/')

4cd2a8c4ea6914f918aa5d7b104dac1e8645e4b8
skredenmathias <skredenmathias@users.noreply.github.com>
2020-11-16 17:11:51 +0100

    try

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,14 +4,14 @@ from app import app
 import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.options import Options
 
-options = webdriver.Options()
-options.binary_location = '/usr/bin/chromium-browser'
+# options = webdriver.Options()
+# options.binary_location = '/usr/bin/chromium-browser'
 
-# All the arguments added for chromium to work on selenium
-options.add_argument("--no-sandbox") #This make Chromium reachable
-options.add_argument("--no-default-browser-check") #Overrides default choices
-options.add_argument("--no-first-run")
-options.add_argument("--disable-default-apps") 
+# # All the arguments added for chromium to work on selenium
+# options.add_argument("--no-sandbox") #This make Chromium reachable
+# options.add_argument("--no-default-browser-check") #Overrides default choices
+# options.add_argument("--no-first-run")
+# options.add_argument("--disable-default-apps") 
 driver = webdriver.Chrome('/home/travis/virtualenv/python3.7   /chromedriver_linux64',chrome_options=options)
 
 
